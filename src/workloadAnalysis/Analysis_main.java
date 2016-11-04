adbff379739418a804193c4b6c29ff84493b5c04
DB <yeodongbin2011@gmail.com>
2016-11-05 00:16:24 +0900

    20161105

--- src/workloadAnalysis/Analysis_main.java
+++ src/workloadAnalysis/Analysis_main.java
@@ -18,64 +18,62 @@ public class Analysis_main {
 		int ReqCount = 0; // Total Request Count (Read +Write)
 		int writeReqCount = 0; // Write Request Count
 		int readReqCount = 0; // Read Request Count
-		
+
 		int totalWritePages = 0;
 		int totalReadPages = 0;
 		int read_1PageCount = 0;
 		int write_1PageCount = 0;
-	 
+
 		long lPN = 0; // logical page number from the workload
 		int pageSize = 0; // physical page size from the workload
-		//int pageOffset = 0;
-		
+		// int pageOffset = 0;
+
 		long tempLong = -1;
-		
+
 		System.out.println(" ** Workload Analysis Start ** \n");
-		
+
 		// LPN, Write-Count , Read-Count
 		TreeMap<Long, Integer> lpnWriteConut = new TreeMap<Long, Integer>();
 		TreeMap<Long, Integer> lpnReadConut = new TreeMap<Long, Integer>();
 		TreeMap<Long, Integer> lpnWritePage = new TreeMap<Long, Integer>();
 		TreeMap<Long, Integer> lpnReadPage = new TreeMap<Long, Integer>();
-		TreeMap<Long, Integer> eachPages = new TreeMap<Long, Integer>();
+		TreeMap<Integer, Integer> eachPages = new TreeMap<Integer, Integer>();
 
 		// I/O trace file format (FILE_INPUT)
 		// .csv:Timestamp,Hostname,DiskNumber,Type,Offset,Size,ResponseTime
-				
-		try (BufferedReader br = new BufferedReader(new FileReader(
-				Config.FINE_INPUT))) {
-			
-			for (String line; (line = br.readLine()) != null;) {
 
+		try (BufferedReader br = new BufferedReader(new FileReader(
+				Config.FILE_INPUT))) {
+
+			for (String line; (line = br.readLine()) != null;) {
 				String[] elements = line.split(",");
-				ReqCount++;
 
 				tempLong = Long.valueOf(elements[4]).longValue();
-				lPN = (int) (tempLong / Config.PAGE_BYPT_SIZE);
+				lPN = (tempLong / Config.PAGE_BYPT_SIZE);
 				// pageOffset = (int) (tempLong % Config.PAGE_BYPT_SIZE);
 
 				tempLong = Long.valueOf(elements[5]).longValue();
-				pageSize = (int) (tempLong / Config.PAGE_BYPT_SIZE);
+				pageSize = (int)(tempLong / Config.PAGE_BYPT_SIZE);
 
 				if (0 == pageSize) {
 					pageSize = 1;
-				} else if (0 < ((int) (tempLong % Config.PAGE_BYPT_SIZE))) {
+				} else if (0 < (tempLong % Config.PAGE_BYPT_SIZE)) {
 					pageSize = pageSize + 1;
 				}
 
+				ReqCount++;
 				if (elements[3].equals("Write")) {
 					writeReqCount++;
 					// write page °¹¼öº° ºÐ¼®(pageSize)
 					if (false == eachPages.containsKey(pageSize)) {
-						eachPages.put(lPN, 1);
+						eachPages.put(pageSize, 1);
 					} else {
-						eachPages.put(lPN, lpnWritePage.get(pageSize)+1);
+						eachPages.put(pageSize, eachPages.get(pageSize) + 1);
 					}
 
 					// write 1page lpn ¸ðÀ¸±â
 					if (pageSize == 1) {
 						write_1PageCount++;
-
 						if (false == lpnWritePage.containsKey(lPN)) {
 							lpnWritePage.put(lPN, 1);
 						} else {
@@ -84,14 +82,13 @@ public class Analysis_main {
 					} else {
 						// in case of multi-pagesÀÇ °æ¿ì ÆäÀÌÁö Á¢±Ù È¸¼ö
 						for (int i = 0; i < pageSize; i++) {
-								totalWritePages++;
+							totalWritePages++;
 
-								if (false == lpnWriteConut.containsKey(lPN + i)) {
-									lpnWriteConut.put(lPN + i, 1);
-								} else {
-									lpnWriteConut.put(lPN + i,
-											lpnWriteConut.get(lPN + i) + 1);
-								}
+							if (false == lpnWriteConut.containsKey(lPN + i)) {
+								lpnWriteConut.put(lPN + i, 1);
+							} else {
+								lpnWriteConut.put(lPN + i,lpnWriteConut.get(lPN + i) + 1);
+							}
 						}
 					}
 				} else if (elements[3].equals("Read")) {
@@ -99,53 +96,58 @@ public class Analysis_main {
 
 					if (pageSize == 1) {
 						read_1PageCount++;
-
+						/*
 						if (false == lpnWritePage.containsKey(lPN)) {
 							lpnReadPage.put(lPN, 1);
 						} else {
 							lpnReadPage.put(lPN, lpnReadPage.get(lPN) + 1);
 						}
+						*/
 					} else {
 						for (int i = 0; i < pageSize; i++) {
 							totalReadPages++;
+							/*
 							if (false == lpnWriteConut.containsKey(lPN + i)) {
 								lpnReadConut.put(lPN + i, 1);
 							} else {
 								lpnReadConut.put(lPN + i,
 										lpnWriteConut.get(lPN + i) + 1);
 							}
+							*/
 						}
 					}
 				}
-				System.out.println("\nplease wait....\n");
 			}
 		} catch (Exception e) {
 			System.out.println("Exception while reading csv file: " + e);
 		}
 
+		
 		MapToTxt(lpnWriteConut, Config.FILE_LPN_WRITE_OUTPUT);
 		MapToTxt(lpnReadConut, Config.FILE_LPN_READ_OUTPUT);
 		MapToTxt(lpnWritePage, Config.FILE_LPN_WRITE_OUTPUT_1PAGE);
-	    MapToTxt(lpnReadPage, Config.FILE_LPN_READ_OUTPUT_1PAGE);
-		MapToTxt(eachPages, Config.FILE_EACH_WRITE_PAGES);
+		MapToTxt(lpnReadPage, Config.FILE_LPN_READ_OUTPUT_1PAGE);
+		intMapToTxt(eachPages, Config.FILE_EACH_WRITE_PAGES);
 
+		
 		FileWriter fw = new FileWriter(new File(Config.FILE_LOG_OUTPUT));
 		BufferedWriter bw = new BufferedWriter(fw);
 
+		bw.newLine(); // ÁÙ¹Ù²Þ
 		bw.write("Total Request Count        = " + ReqCount);
 		bw.newLine(); // ÁÙ¹Ù²Þ
 		bw.write("Write Request Count        = " + writeReqCount);
 		bw.newLine(); // ÁÙ¹Ù²Þ
 		bw.write("Read Request Count         = " + readReqCount);
-		bw.write("\n\n-----------------------------------------\n\n");
+		bw.write("\n-----------------------------------------\n");
 		bw.write("Total Write Pages Count    = " + totalWritePages);
 		bw.newLine(); // ÁÙ¹Ù²Þ
 		bw.write("Total Read Pages Count     = " + totalReadPages);
-		bw.write("\n\n-----------------------------------------\n\n");
+		bw.write("\n-----------------------------------------\n");
 		bw.write("Write 1-Page Request Count = " + write_1PageCount);
 		bw.newLine(); // ÁÙ¹Ù²Þ
 		bw.write("Read 1-Page Request Count  = " + read_1PageCount);
-		bw.write("\n\n-----------------------------------------\n\n");
+		bw.write("\n-----------------------------------------\n");
 		bw.write("LPN WRITE LIST SIZE        = " + lpnWriteConut.size());
 		bw.newLine(); // ÁÙ¹Ù²Þ
 		bw.write("LPN READ LIST SIZE         = " + lpnReadConut.size());
@@ -153,15 +155,12 @@ public class Analysis_main {
 		bw.write("LPN WRITE LIST SIZE(1page) = " + lpnWritePage.size());
 		bw.newLine(); // ÁÙ¹Ù²Þ
 		bw.write("LPN READ LIST SIZE(1page)  = " + lpnReadPage.size());
-		bw.newLine(); // ÁÙ¹Ù²Þ
-
 		bw.close();
 		fw.close();
 
 		System.out.println("** Workload Analysis End **");
 	}
 
-	
 	public static void MapToTxt(int[][] map, String output) throws IOException {
 		// create your file writer and buffered reader
 		FileWriter fw = new FileWriter(new File(output));
@@ -176,6 +175,29 @@ public class Analysis_main {
 		return;
 	}
 
+	public static void intMapToTxt(Map<Integer, Integer> map, String output)
+			throws IOException {
+
+		// create your file writer and buffered reader
+		FileWriter fstream = new FileWriter(new File(output));
+		BufferedWriter bw = new BufferedWriter(fstream);
+
+		// create your iterator for your map
+		Iterator<Entry<Integer, Integer>> it = map.entrySet().iterator();
+
+		while (it.hasNext()) {
+			// the key/value pair is stored here in pairs
+			Entry<Integer, Integer> pairs = it.next();
+			// since you only want the value, we only care about
+			// pairs.getValue(), which is written to out
+			bw.write(pairs.getKey() + "	" + pairs.getValue() + "\n");
+		}
+
+		// lastly, close the file and end
+		bw.close();
+		return;
+	}
+	
 	
 	public static void MapToTxt(Map<Long, Integer> map, String output)
 			throws IOException {
@@ -192,7 +214,7 @@ public class Analysis_main {
 			Map.Entry<Long, Integer> pairs = it.next();
 			// since you only want the value, we only care about
 			// pairs.getValue(), which is written to out
-			bw.write(pairs.getKey() + "," + pairs.getValue() + "\n");
+			bw.write(pairs.getKey() + "	" + pairs.getValue() + "\n");
 		}
 
 		// lastly, close the file and end
--- src/workloadAnalysis/Config.java
+++ src/workloadAnalysis/Config.java
@@ -3,8 +3,7 @@ package workloadAnalysis;
 public class Config {
 
 	public static int PAGE_BYPT_SIZE = 4096; // 4KB = 4096
-	public static String FINE_INPUT = "D:/수업 자료/0. 연구 자료/Traces_MSR/"
-			+ "msr-cambridge2/MSR-Cambridge/web_0.csv/CAMRESWEBA03-lvm0.csv";
+	public static String FILE_INPUT = "C:/Users/ajou/git/workloadAnalysis/CAMRESWMSA03-lvm0.csv";
 //	public static String FINE_INPUT = "D:/수업 자료/0. 연구 자료/Traces_MSR/"
 //			+ "msr-cambridge2/MSR-Cambridge/web_0.csv/CAMRESWEBA03-lvm0.csv";
 	
