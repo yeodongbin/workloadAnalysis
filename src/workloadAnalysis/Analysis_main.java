d270bbefdecab8793101bc0f23c2b4808e62d5c7
DB <yeodongbin2011@gmail.com>
2016-11-10 23:10:04 +0900

    2016-11-10 Avg Interval

--- src/workloadAnalysis/Analysis_main.java
+++ src/workloadAnalysis/Analysis_main.java
@@ -14,7 +14,6 @@ import java.util.TreeMap;
 public class Analysis_main {
 
 	public static void main(String args[]) throws Exception {
-
 		System.out.println(" ** Workload Analysis Start ** \n");
 
 		switch (Config.WORK_LOAD) {
@@ -22,10 +21,10 @@ public class Analysis_main {
 			msrWorkload();
 			break;
 		case 2:
-			oltpWorkload();//have to evaluate!
+			oltpWorkload();// have to evaluate!
 			break;
 		case 3:
-			intervalAnalysis();//have to evaluate!
+			intervalAnalysis();// have to evaluate!
 			break;
 		default:
 			System.out.println("해당 파일이 없습니다.");
@@ -34,31 +33,31 @@ public class Analysis_main {
 
 		System.out.println("** Workload Analysis End **");
 	}
-	
-	
-	public static void intervalAnalysis() throws IOException {
-		
-		int writeReq1Page = 0;
-		int lastReq1Page = 0;
 
+	public static void intervalAnalysis() throws IOException {
+		int req = 0;
+		int writeReq = 0;
+		int writeReq1Page = 0;
+		
+		int numOfItem = 0;
+		int sumOfAvgInterval = 0;
+		
 		long lPN = 0; // logical page number from the workload
 		int pageSize = 0; // physical page size from the workload
-		
 		long tempLong = -1;
 
 		// LPN, Write-Count , Read-Count
 		TreeMap<Long, Integer> lpnLastTime = new TreeMap<Long, Integer>();
 		TreeMap<Long, Integer> write1PageCount = new TreeMap<Long, Integer>();
 		TreeMap<Long, Integer> sumIntervalNum = new TreeMap<Long, Integer>();
+		TreeMap<Long, Float> avgIntervalNum = new TreeMap<Long, Float>();
 		
-
 		// I/O trace file format (FILE_INPUT)
 		// .csv:Timestamp,Hostname,DiskNumber,Type,Offset,Size,ResponseTime
-
 		try (BufferedReader br = new BufferedReader(new FileReader(
 				Config.FILE_INPUT))) {
 			for (String line; (line = br.readLine()) != null;) {
-				
+				req++;
 				String[] elements = line.split(",");
 
 				tempLong = Long.valueOf(elements[4]).longValue();
@@ -72,22 +71,20 @@ public class Analysis_main {
 				} else if (0 < (tempLong % Config.PAGE_BYPT_SIZE)) {
 					pageSize = pageSize + 1;
 				}
-				
-				reqCount++;
-				
+
 				if (elements[3].equals("Write")) {
-					writeReqCount++;
-					
+					writeReq++;
+
 					// write 1page lpn 모으기
 					if (pageSize == 1) {
-
 						writeReq1Page++;
+
 						// interval 총량
 						if (false == sumIntervalNum.containsKey(lPN)) {
 							sumIntervalNum.put(lPN, 0);
 						} else {
-							sumIntervalNum.put(lPN,
-									writeReq1Page - lpnLastTime.get(lPN));
+							sumIntervalNum.put(lPN, sumIntervalNum.get(lPN)
+									+ (writeReq1Page - lpnLastTime.get(lPN)));
 						}
 
 						// 최근 1page write 접근 시간
@@ -107,37 +104,64 @@ public class Analysis_main {
 			System.out.println("Exception while reading csv file: " + e);
 		}
 
-		TreeMap<Long, Float> avgIntervalNum = new TreeMap<Long, Float>();
-		for (int i = 0; i < avgIntervalNum.size(); i++) {
-			avgIntervalNum
-					.put(lPN,
-							(float) (sumIntervalNum.get(lPN) / write1PageCount
-									.get(lPN)));
+		//Average of Interval each LPN 
+		for (Long key : write1PageCount.keySet()) {
+			if (1 != write1PageCount.get(key)) {
+				avgIntervalNum.put(key,
+						(float) (sumIntervalNum.get(key) / (write1PageCount
+								.get(key) - 1)));
+				numOfItem++;
+			}
 		}
 
-		MapToTxt(write1PageCount, Config.FILE_LPN_WRITE_OUTPUT_1PAGE);
-		floatMapToTxt(avgIntervalNum, Config.FILE_LPN_WRITE_OUTPUT_1PAGE);
+		//Total average of Interval 
+		for (Long key : avgIntervalNum.keySet()) {
+			sumOfAvgInterval += avgIntervalNum.get(key);
+		}
+
+		MapToTxt(lpnLastTime, "lpnLastTime.txt");
+		MapToTxt(write1PageCount, "write1PageCount.txt");
+		MapToTxt(sumIntervalNum, "sumIntervalNum.txt");
+		floatMapToTxt(avgIntervalNum, Config.FILE_AVG_INTERVAL);
+		
+		System.out.println(">> Request Count : " + req);
+		System.out.println("> Write Request           : " + writeReq);
+		System.out.println("> Write Request (Sequtial): "
+				+ (writeReq - writeReq1Page));
+		System.out.println("> Write Request (Random)  : " + writeReq1Page);
+		// System.out.println("> Read Request :" + readReq);
+		System.out.println("\n> Sum of Avg Interval     : " + sumOfAvgInterval);
+		System.out.println("> Number of Avg Interval  : " + numOfItem);
+		System.out.println(">> Total Average of Interval : "
+				+ (sumOfAvgInterval / numOfItem));
+		System.out.println("");
 	}
-	
+
 	
 	public static void oltpWorkload() throws IOException {
+		int writeReq = 0;
+		int writeReq1Page = 0;
+		int readReq = 0;
 
 		int size = 0;
+		int req = 0;
 		long startLPN = 0;
 		long endLPN = 0;
 		long tempLong = 0;
 		long tempLong2 = 0;
 
+		TreeMap<Long, Integer> lpnLastTime = new TreeMap<Long, Integer>();
+		TreeMap<Long, Integer> write1PageCount = new TreeMap<Long, Integer>();
+		TreeMap<Long, Integer> sumIntervalNum = new TreeMap<Long, Integer>();
+
 		FileWriter fw = new FileWriter(new File(Config.FILE_OUTPUT));
 		BufferedWriter bw = new BufferedWriter(fw);
 
 		try (BufferedReader br = new BufferedReader(new FileReader(
-				Config.FILE_INPUT))) {
-
+				Config.FILE_OLTP_INPUT))) {
 			for (String line; (line = br.readLine()) != null;) {
-
 				String[] elements = line.split(",");
-
+				req++;
 				tempLong = Long.valueOf(elements[1]).longValue();
 				tempLong2 = Long.valueOf(elements[2]).longValue();
 
@@ -145,18 +169,54 @@ public class Analysis_main {
 						((tempLong * Config.LBA_SIZE) / Config.PAGE_BYPT_SIZE))
 						.longValue();
 
-				endLPN = Long.valueOf(
-						((tempLong * Config.LBA_SIZE) / Config.PAGE_BYPT_SIZE)
-								+ tempLong2).longValue();
+				endLPN = Long
+						.valueOf(
+								(((tempLong * Config.LBA_SIZE) + tempLong2) / Config.PAGE_BYPT_SIZE))
+						.longValue();
 
 				size = (int) ((endLPN - startLPN) + 1);
 
-				if ("w" == elements[3]) {
+				if (1048576 < endLPN) {
+				}
+
+				// Write Read 분석
+				if (elements[3].equals("w")) {
 					bw.write("Write" + "," + startLPN + "," + size);
-					bw.newLine(); // 줄바꿈
-				} else if ("r" == elements[3]) {
+					bw.newLine();
+					writeReq++;
+
+					// average Interval Calculation
+					if (size == 1) {
+						writeReq1Page++;
+
+						// interval 총량
+						if (false == sumIntervalNum.containsKey(startLPN)) {
+							sumIntervalNum.put(startLPN, 0);
+						} else {
+							sumIntervalNum.put(
+									startLPN,
+									sumIntervalNum.get(startLPN)
+											+ (writeReq1Page - lpnLastTime
+													.get(startLPN)));
+						}
+
+						// 최근 1page write 접근 시간
+						lpnLastTime.put(startLPN, writeReq1Page);
+
+						// LPN별 1page write 횟수
+						if (false == write1PageCount.containsKey(startLPN)) {
+							write1PageCount.put(startLPN, 1);
+						} else {
+							write1PageCount.put(startLPN,
+									(int) write1PageCount.get(startLPN) + 1);
+						}
+					}
+
+				} else if (elements[3].equals("r")) {
 					bw.write("Read" + "," + startLPN + "," + size);
 					bw.newLine(); // 줄바꿈
+					readReq++;
+					// System.out.println("Read" + "," + startLPN + "," + size);
 				}
 			}
 		} catch (Exception e) {
@@ -165,11 +225,44 @@ public class Analysis_main {
 
 		bw.close();
 		fw.close();
-	}
-	
-	
-	public static void msrWorkload() throws IOException {
 
+		TreeMap<Long, Float> avgIntervalNum = new TreeMap<Long, Float>();
+
+		int numOfItem = 0;
+		int sumOfAvgInterval = 0;
+
+		for (Long key : write1PageCount.keySet()) {
+			if (1 != write1PageCount.get(key)) {
+				avgIntervalNum.put(key,
+						(float) (sumIntervalNum.get(key) / (write1PageCount
+								.get(key) - 1)));
+				numOfItem++;
+			}
+		}
+
+		for (Long key : avgIntervalNum.keySet()) {
+			sumOfAvgInterval += avgIntervalNum.get(key);
+		}
+
+		MapToTxt(lpnLastTime, "lpnLastTime.txt");
+		MapToTxt(write1PageCount, "write1PageCount.txt");
+		MapToTxt(sumIntervalNum, "sumIntervalNum.txt");
+		floatMapToTxt(avgIntervalNum, Config.FILE_AVG_INTERVAL);
+
+		System.out.println(">> Request Count: " + req);
+		System.out.println("> Write Request : " + writeReq);
+		System.out.println("> Write Request (Sequtial): "
+				+ (writeReq - writeReq1Page));
+		System.out.println("> Write Request (Random)  : " + writeReq1Page);
+		// System.out.println("> Read Request :" + readReq);
+		System.out.println("\n> Sum of Avg Interval    : " + sumOfAvgInterval);
+		System.out.println("> Number of Avg Interval : " + numOfItem);
+		System.out.println(">> Total Average of Interval : "
+				+ (sumOfAvgInterval / numOfItem));
+		System.out.println("");
+	}
+
+	public static void msrWorkload() throws IOException {
 		int ReqCount = 0; // Total Request Count (Read +Write)
 		int writeReqCount = 0; // Write Request Count
 		int readReqCount = 0; // Read Request Count
@@ -323,7 +416,6 @@ public class Analysis_main {
 
 	public static void intMapToTxt(Map<Integer, Integer> map, String output)
 			throws IOException {
-
 		// create your file writer and buffered reader
 		FileWriter fstream = new FileWriter(new File(output));
 		BufferedWriter bw = new BufferedWriter(fstream);
@@ -346,7 +438,6 @@ public class Analysis_main {
 
 	public static void floatMapToTxt(Map<Long, Float> map, String output)
 			throws IOException {
-
 		// create your file writer and buffered reader
 		FileWriter fstream = new FileWriter(new File(output));
 		BufferedWriter bw = new BufferedWriter(fstream);
@@ -369,7 +460,6 @@ public class Analysis_main {
 
 	public static void MapToTxt(Map<Long, Integer> map, String output)
 			throws IOException {
-
 		// create your file writer and buffered reader
 		FileWriter fstream = new FileWriter(new File(output));
 		BufferedWriter bw = new BufferedWriter(fstream);
--- src/workloadAnalysis/Config.java
+++ src/workloadAnalysis/Config.java
@@ -2,20 +2,20 @@ package workloadAnalysis;
 
 public class Config {
 
-	public static int WORK_LOAD = 2; 
-	//1:MSR , 2:oltp financial1??, 3: Interval Analysis??
+	public static int WORK_LOAD = 3; 
+	//1:MSR , 2:oltp financial1, 3: Interval Analysis_MSR
 	
 	public static int LBA_SIZE = 512; // 
 	public static int PAGE_BYPT_SIZE = 4096; // 4KB = 4096
 	public static int LOGICAL_BLOCK_NUM = 4096;
+	public static int LOGICAL_PAGE_NUM = 256;
 	
-	public static String FILE_INPUT = "./Financial1.spc";
-//	public static String FILE_INPUT = "C:/Users/ajou/git/workloadAnalysis/CAMRESWMSA03-lvm0.csv";
+	
+	// 1: msr
+	public static String FILE_INPUT = "C:/Users/ajou/git/workloadAnalysis/CAMRESWMSA03-lvm0.csv";
 //	public static String FINE_INPUT = "D:/수업 자료/0. 연구 자료/Traces_MSR/"
 //			+ "msr-cambridge2/MSR-Cambridge/web_0.csv/CAMRESWEBA03-lvm0.csv";
 	
-	
-	public static String FILE_OUTPUT = "./Financial1.txt";
 	public static String FILE_LOG_OUTPUT = "./FILE_LOG_OUTPUT.txt";
 	public static String ACCESS_TIMES = "./ACCESS_TIMES.txt";
 
@@ -23,6 +23,14 @@ public class Config {
 	public static String FILE_LPN_WRITE_OUTPUT_1PAGE = "./FILE_LPN_WRITE_OUTPUT_1PAGE.txt";
 	public static String FILE_LPN_READ_OUTPUT = "./FILE_LPN_READ_OUTPUT.txt";
 	public static String FILE_LPN_READ_OUTPUT_1PAGE = "./FILE_LPN_READ_OUTPUT_1PAGE.txt";
-	
 	public static String FILE_EACH_WRITE_PAGES = "./FILE_EACH_WRITE_PAGES.txt";
+	
+	// 2: oltp finanical1 
+	public static String FILE_OLTP_INPUT = "./Financial1.txt"
+			+ "";
+	public static String FILE_OUTPUT = "./Financial1_out.txt";
+	
+	// 3: Interval 
+	public static String FILE_AVG_INTERVAL = "./FILE_AVG_INTERVAL.txt";
+	
 }
